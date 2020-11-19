48564494254250e2e9e7b0ede89062f1774fcd46
Casperoazanda <casperoazanda@163.com>
2020-11-19 15:46:59 -0800

    Updates

---
+++ precache-manifest.f27e0b3f9a5e0ed7c8a88fe28d7210f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52876097396370b424645a5077d3d430",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0548f037023e892b75a",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "e4d8b105b5b3d37e3c46",
+    "url": "/static/js/2.75934f72.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.75934f72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0548f037023e892b75a",
+    "url": "/static/js/main.c2868a73.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "9b35ec30d32b763533faccfdc82277a6",
+    "url": "/static/media/azanda.9b35ec30.jpg"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
